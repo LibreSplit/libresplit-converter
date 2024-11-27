130a9dd141998032b0a48b4fefb067b74b598a19
JackTench <79285604+JackTench@users.noreply.github.com>
2024-11-27 18:38:29 +0000

    Implement basic fetching of game name and category.

--- wasm-lib/src/livesplit.rs
+++ wasm-lib/src/livesplit.rs
@@ -1,3 +1,5 @@
+use spex::xml::XmlDocument;
+
 pub struct LiveSplitFile {
     game_name: String,
     category_name: String,
@@ -8,4 +10,18 @@ pub struct LiveSplitFile {
 
 pub struct Segment {
     split_time: String,
+}
+
+pub fn read(file: XmlDocument) -> String {
+    let mut buf = String::new();
+
+    // Read game name.
+    let game_name = file.root().req("GameName").element().unwrap();
+    buf.push_str(game_name.text().unwrap());
+
+    // Read category name.
+    let category_name = file.root().req("CategoryName").element().unwrap();
+    buf.push_str(category_name.text().unwrap());
+
+    buf
 }
