410fc7d48b0a8c2234449f7902267165cb3ce731
JackTench <79285604+JackTench@users.noreply.github.com>
2024-11-27 22:53:42 +0000

    Read category, platform and attempt count.

--- wasm-lib/src/livesplit.rs
+++ wasm-lib/src/livesplit.rs
@@ -13,13 +13,34 @@ pub struct Segment {
 }
 
 pub fn read(file: XmlDocument) -> String {
-
     // Read game name.
     let elm_game_name = file.root().opt("GameName").element();
     let game_name = match elm_game_name {
         Some(name) => name.text().expect("Unknown Game"),
         None => "Unknown Game",
     };
+
+    // Read category.
+    let elm_category_name = file.root().opt("CategoryName").element();
+    let category_name = match elm_category_name {
+        Some(category) => category.text().expect("Unknown Category"),
+        None => "Unknown Category",
+    };
+
+    // Read platform.
+    let elm_platform = file.root().opt("Platform").element();
+    let platform  = match elm_platform {
+        Some(plat) => plat.text().expect("Unknown Platform"),
+        None => "Unknown Platform",
+    };
+
+    // Read attempt count.
+    let elm_attempt_count = file.root().opt("AttemptCount").element();
+    let attempt_count_str = match elm_attempt_count {
+        Some(count_str) => count_str.text().expect("0"),
+        None => "0"
+    };
+    let attempt_count: u32 = attempt_count_str.trim().parse().unwrap_or(0);
 
     game_name.to_string()
 }
