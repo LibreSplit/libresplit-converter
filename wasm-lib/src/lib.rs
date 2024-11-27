461613833badcaa03e2d403f20d4ccf7c2e43aa0
JackTench <79285604+JackTench@users.noreply.github.com>
2024-11-27 23:13:00 +0000

    Remove boilerplate add function.

--- wasm-lib/src/lib.rs
+++ wasm-lib/src/lib.rs
@@ -6,11 +6,6 @@ use wasm_bindgen::prelude::*;
 mod livesplit;
 
 #[wasm_bindgen]
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
-
-#[wasm_bindgen]
 pub fn convert(file: String) -> String {
     let cursor = Cursor::new(file);
     let xml = XmlReader::parse_auto(cursor).unwrap();
